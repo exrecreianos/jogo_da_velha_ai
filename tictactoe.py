4bda4f5423b060b921e8b4589f886bf53a237dc0
Thiago da Mota Souza <mtsz.thiago@gmail.com>
2019-04-06 21:28:01 -0300

    separating player from environment logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 from random import randint
 
+from players import human_player, rand_player
 
 class tictactoe():
     
@@ -8,26 +9,28 @@ class tictactoe():
                     '4','5','6',
                     '7','8','9']
         self.turn = 1 # 1 - Player 1 | 2 - Player 2
-        self.p_char = 'X'
-        self.c_char = 'O'
         self.player1 = player1
         self.player2 = player2
-        self.playerToPlay = 0
+        self.palyersSigns = [player1.sign, player2.sign]
 
     def winner(self):
-        for i in ['X','O']:
-            # horizontal
-            if self.board[0] == self.board[1] == self.board[2] == i: return i
-            if self.board[3] == self.board[4] == self.board[5] == i: return i
-            if self.board[6] == self.board[7] == self.board[8] == i: return i
-            # vertical
-            if self.board[0] == self.board[3] == self.board[6] == i: return i
-            if self.board[1] == self.board[4] == self.board[7] == i: return i
-            if self.board[2] == self.board[5] == self.board[8] == i: return i
-            # diagonal
-            if self.board[0] == self.board[4] == self.board[8] == i: return i
-            if self.board[6] == self.board[4] == self.board[2] == i: return i
-        return None
+        pl = self.getPlayerOfTurn()
+        # horizontal
+        if self.board[0] == self.board[1] == self.board[2] == pl.sign: return True
+        if self.board[3] == self.board[4] == self.board[5] == pl.sign: return True
+        if self.board[6] == self.board[7] == self.board[8] == pl.sign: return True
+        # vertical
+        if self.board[0] == self.board[3] == self.board[6] == pl.sign: return True
+        if self.board[1] == self.board[4] == self.board[7] == pl.sign: return True
+        if self.board[2] == self.board[5] == self.board[8] == pl.sign: return True
+        # diagonal
+        if self.board[0] == self.board[4] == self.board[8] == pl.sign: return True
+        if self.board[6] == self.board[4] == self.board[2] == pl.sign: return True
+        return False
+
+    def endGame(self):
+        possible = [num for num in self.board if num.isdigit()]
+        return len(possible) <= 0
 
     def view(self):
         print(" %s | %s | %s " % (self.board[0],self.board[1],self.board[2]))
@@ -36,57 +39,69 @@ class tictactoe():
         print("---+---+---")
         print(" %s | %s | %s " % (self.board[6],self.board[7],self.board[8]))
 
-    def move(self,pos):
+    def assertIfMoveIsLegal(self, move):
+        if not move: return False
+        if not 0 < move < 10: return False
+        if self.board[move-1] in self.palyersSigns: return False
+        return True
 
-        if not pos: return None
-        if not 0 < pos < 10: return False
-        if self.board[pos-1] in ['X','O']: return False
+    def getPlayerOfTurn(self):
+        return (self.player2,self.player1)[self.turn == 1]
 
-        self.board[pos-1] = ('O','X')[self.turn == 1]
+    def updateBoard(self,pos):
+        p = self.getPlayerOfTurn()
+        self.board[pos-1] = p.sign
+
+    def nextTurn(self):
         self.turn = (1,2)[self.turn == 1]
     
+    def showEndGameMessage(self, winner):
+        if winner:
+            p = self.getPlayerOfTurn()
+            self.view()
+            print("%s Wins!" % p.name)
+        else:
+            self.view()
+            print("Draw!")
+
+    def processLegalMove(self, movement):
+        self.updateBoard(movement)
+        win = self.winner()
+        if win:
+            self.showEndGameMessage(win)
+            exit(0)
+
+        self.nextTurn()
+
+    def processIlegalMove(self, movement):
+        print("ilegal move..try again")
+
     def play(self):
         while True:
+
+            # assert if game has ended on previous loop
+            endGame = self.endGame()
+            if endGame:
+                self.showEndGameMessage(False)
+                exit(0)
+
             self.view()
-
             print("\nPlayer {}: ".format(self.turn))
-            if self.turn == 1:
-                if not self.player1 is None:
-                    movement = self.player1.play(self.board)
-                else:
-                    movement = int(input())
-            else:
-                if not self.player2 is None:
-                    movement = self.player2.play(self.board)
-                else:
-                    movement = int(input())
+
+            # wait for player input
+            pl = self.getPlayerOfTurn()
+            movement = pl.play(self.board)
             print("")
-            self.move(movement)
 
-            win = self.winner()
-            if not win: continue
-            if win == self.p_char:
-                self.view()
-                print("\nPlayer 1 Wins!")
-                input()
-                exit(0)
-            if win == self.c_char:
-                self.view()
-                print("\nPlayer 2 Wins!")
-                input()
-                exit(0)
+            # process move
+            legalMove = self.assertIfMoveIsLegal(movement)
+            if legalMove:
+                self.processLegalMove(movement)
+            else:
+                self.processIlegalMove(movement)
 
+P1 = human_player("P1", 'X')
+P2 = rand_player('P2', "O")
 
-class player():
-    def __init__(self, name='P1'):
-        self.name = name
-
-    def play(self, board):
-        possible = [t for t in board if t != 'X' or t != 'O' ]
-        movement = randint(0, len(possible))
-        return int(possible[movement])
-
-P1 = player("P1")
-# P2 = player("P2")
-game = tictactoe(player1=P1)
+game = tictactoe(player1=P1, player2=P2)
 game.play() 
